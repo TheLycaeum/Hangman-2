1ccf04d506bb830d8ceeec8f97df38f59ae8d8de
pavananms <pavananms@gmail.com>
2019-02-12 13:41:20 +0530

    Added try again option

--- hangman.py
+++ hangman.py
@@ -1,7 +1,5 @@
 # hangman.py
-
 import random
-
 
 
 def get_secret_word(word_file="/usr/share/dict/words"):
@@ -9,22 +7,25 @@ def get_secret_word(word_file="/usr/share/dict/words"):
         good_words = []
         for i in f:
             i = i.strip()
-            if len(i) <= 6:     # No short words
+            if len(i) <= 6:      # No short words
                 continue
-            if not i.isalpha(): # No punctuation
+            if not i.isalpha():  # No punctuation
                 continue
-            if i[0].isupper():  # No proper nouns
+            if i[0].isupper():   # No proper nouns
                 continue
             good_words.append(i)
     return random.choice(good_words)
 
+
 def mask_line(word):
     return "*"*len(word)
+
 
 def tries_left(tri):
     return 10 - tri
 
-def guess_letter(word,letter,mask_line):
+
+def guess_letter(word, letter, mask_line):
     if letter in word:
         x = 0
         for i in word:
@@ -35,40 +36,60 @@ def guess_letter(word,letter,mask_line):
     else:
         return False
 
-game = True
-word = get_secret_word()
-#print(word)
-mask = mask_line(word)
-x = 1
-max_guess = 10
-guesses = []
-while game:
-    print(mask)
-    print(f"No: of guesses left {tries_left(x)}\n\n")
-    letter = input("Your guess : ").lower()
-    if letter in guesses:
-        print("already guessed : {guesses} ")
-        continue
-    guess = guess_letter(word,letter,mask)
-    if guess == False:
-        print(mask)
-        print("Wrong guess")
-        guesses.append(letter)
-        x = x + 1
-    else:
-        mask = guess
-        print(mask)
-    if mask == word:
-        game = False
-        print(f"Congrats, you won !!! word was {word}")
-    else:
-        print(f"guesses: {sorted(guesses)}\n\n")
-        if x >= 10:
-            print(f"Sorry, 10 guesses over, the word was : {word}")
+
+def play():
+    game = True
+    word = get_secret_word()
+    mask = mask_line(word)
+    x = 1
+    guesses = []
+    while game:
+        print(mask, end="\n\n")
+        print(f"Number of guesses left {tries_left(x)}\n\n")
+        letter = input("Your guess : ").lower()
+        if letter in guesses:
+            print(f"already guessed : {guesses} ")
+            continue
+        guess = guess_letter(word, letter, mask)
+        if guess == False:
+            print("Wrong guess")
+            guesses.append(letter)
+            x = x + 1
+        else:
+            mask = guess
+            print(mask)
+        if mask == word:
             game = False
+            print(f"Congrats, you won !!! word was {word}")
+        else:
+            print(f"guesses: {sorted(guesses)}\n\n")
+            if x >= 10:
+                print(f"Sorry, 10 guesses over, the word was : {word}")
+                game = False
 
 
+def loop_play():
+    print("""
+          Hi this is Pavanan's hangman game !
+          Rules:
+          - You have 10 tries to guess the word.
+          - Your word is hidden using "*"'s. number of stars is same as the
+            number of letters in the word
+          - Enter one letter as guess in each chance.
+          - After each guess, a list of previous guesses will be shown in
+            alphabetical order.
+          - If you want to continue playing, press 'y' after the game !
+            \n\n\n""")
+    play()
+    loop = True
+    while loop:
+        ans = input("\nDo you want to try again ?(y/n) ")
+        if ans == "y":
+            print("starting again \n\n")
+            play()
+        else:
+            loop = False
+            print("Exiting.")
 
 
-
-
+loop_play()
